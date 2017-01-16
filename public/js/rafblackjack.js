978ef2944a6186dedc026c277e754233c3a8164a
Rafiq Shahsawary <rshahsawary1@hotmail.com>
2017-01-16 16:12:21 +0000

    card names etc done. showing images in card divs

--- public/js/rafblackjack.js
+++ public/js/rafblackjack.js
@@ -1,24 +1,54 @@
-let hearts = ["Ah", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "Jh", "Qh", "Kh"];
-let diamonds = ["Ad", "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "Jd", "Qd", "Kd"];
-let spades = ["As",  "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "Js", "Qs", "Ks"];
-let clubs = ["Ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "Jc", "Qc", "Kc"];
-let imgArray = [];
-let deck = $.merge($.merge(hearts, diamonds), $.merge(spades, clubs));
+const hearts = ["Ah", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "Jh", "Qh", "Kh"];
+const diamonds = ["Ad", "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "Jd", "Qd", "Kd"];
+const spades = ["As",  "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "Js", "Qs", "Ks"];
+const clubs = ["Ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "Jc", "Qc", "Kc"];
+const subDeck = $.merge($.merge(hearts, diamonds), $.merge(spades, clubs));
+const deck = subDeck.concat(subDeck).concat(subDeck);
 let createImagesArray = () => {
-    for(let j=0; j<53; j++){
+    let imgArray = [];
+    for(let j=0; j<52; j++){
         img = new Image();
-        img.src = "../images/" + j;
-    } 
+        img.src = `../card_images/${deck[j]}.png`;
+        imgArray.push(img);
+    }
+    console.log("image array : ");
+    console.log(imgArray);
+    return imgArray;
 }
-// this produces 3 decks
 
-// remember to take mod 52 as the img array only goes to 52
+let shuffle = (deck) => {
+    let m = deck.length;
+    let currentElement; 
+    let index;
+    // While there remain elements to shuffle…
+    while(m){
+        // Pick a remaining element at the end of the array
+        index = Math.floor(Math.random() * m--);
+        // store the end-unshuffled element in currentElement
+        currentElement = deck[m];
+        // assign a random element (array[i]) in the current position
+        // so shuffled item are at the end of the array
+        deck[m] = deck[index];
+        // assign the random position the original element
+        deck[index] = currentElement;
+    }
+    console.log(deck);
+    return deck;
+    }
+// this produces 3 decks
 let finalDeck = deck.concat(deck).concat(deck);
 
 let deal = () => {
-    // random numbers [0, 155]
-    let rand = Math.floor(Math.random() * 156)
+    let imgArray = createImagesArray();
+    let dealID = ["#0", "#1", "#5", "#6"];
+    for(let j=0; j<4; j++){
+        let id = dealID[j];
+        // random num between [0, 155] - 3 decks - take %52 because image arry is [0, 52]
+        let rand = Math.floor(Math.random() * 156)%52;
+        $(id).html(imgArray[rand]);
+    }    
 }
 $(() => {
-    console.log(finalDeck);
+    shuffle(deck);
+    deal();
 })
