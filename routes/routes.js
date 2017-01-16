a0522ae6651f34151bb1bcf41115cf4b38bc7354
andreea grigorescu <deea26@gmail.com>
2017-01-16 15:50:37 +0000

    added a post request for the registration. intergrated vue.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,12 +4,14 @@ const registrationController = require("../controllers/registrationController")
 const showGameController = require("../controllers/showGameController")
 const loginController = require("../controllers/loginController")
 const homepageController = require("../controllers/homepageController")
-
+// const registerController = require()
 
 router.get("/", homepageController.showHomepage);
 router.get("/login", loginController.showLogin);
 router.get("/registration", registrationController.showRegistration);
-
 router.get("/play", showGameController.showGameBoard); 
+
+
+router.post("/registration");
 
 module.exports = router;
