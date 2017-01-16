cc0663d3b30e5fcc60b75e83c6141270626573ba
andreea grigorescu <deea26@gmail.com>
2017-01-16 12:40:01 +0000

    added a submit button

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const registrationController = require("../controllers/registrationController")
 const showGameController = require("../controllers/showGameController")
+const homepageController = require("../controllers/homepageController")
 
 
-router.get("/", registrationController.showRegistration);
+router.get("/", homepageController.showHomepage);
+router.get("/login");
+router.get("/registration", registrationController.showRegistration);
 router.get("/play", showGameController.showGameBoard); 
 
 
