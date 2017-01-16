0b05e0cee4ded7ef3336275711d92f9e60057858
Tech-A37 <Tech-A37@Tech-A37s-MacBook-Air.local>
2017-01-16 12:48:47 +0000

    imrans updated version

--- routes/routes.js
+++ routes/routes.js
@@ -2,17 +2,12 @@ const express = require("express");
 const router = express.Router();
 const registrationController = require("../controllers/registrationController")
 const showGameController = require("../controllers/showGameController")
-
 const loginController = require("../controllers/loginController")
-
-router.get("/", registrationController.showRegistration);
-router.get("/login", loginController.showLogin);
-
 const homepageController = require("../controllers/homepageController")
 
 
 router.get("/", homepageController.showHomepage);
-router.get("/login");
+router.get("/login", loginController.showLogin);
 router.get("/registration", registrationController.showRegistration);
 
 router.get("/play", showGameController.showGameBoard); 
