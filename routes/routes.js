76d48307f7b9cc1683db7ee5a88964074ddb9c74
Simeron Taak <simeron03@icloud.com>
2017-01-16 12:12:15 +0000

    gameBoard files have been added - ejs, css, routes and controller, this is just a rough basic page

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const registrationController = require("../controllers/registrationController")
+const showGameController = require("../controllers/showGameController")
 
 
 router.get("/", registrationController.showRegistration);
+router.get("/play", showGameController.showGameBoard); 
 
 
 module.exports = router;
