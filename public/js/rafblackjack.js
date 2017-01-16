7b588d53f4ebd22e1423c90955fef86aa699e1f2
Rafiq Shahsawary <rshahsawary1@hotmail.com>
2017-01-16 22:25:45 +0000

    player twist functioning with no restriction or chronology

--- public/js/rafblackjack.js
+++ public/js/rafblackjack.js
@@ -1,52 +1,70 @@
+// ***** MAXIMUM 5 cards        ***** //
+// ***** ACES HIGH              ***** //
+// ***** 3 DECKS OF CARDS       ***** //
+// ***** NO SPLITS              ***** //
+// ***** DEALER TWISTS FOR < 17 ***** //
 const hearts = ["Ah", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "Jh", "Qh", "Kh"];
 const diamonds = ["Ad", "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "Jd", "Qd", "Kd"];
 const spades = ["As",  "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "Js", "Qs", "Ks"];
 const clubs = ["Ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "Jc", "Qc", "Kc"];
-const subDeck = $.merge($.merge(hearts, diamonds), $.merge(spades, clubs));
-const deck = subDeck.concat(subDeck).concat(subDeck);
+const oneDeck = $.merge($.merge(hearts, diamonds), $.merge(spades, clubs));
+const deck = oneDeck.concat(oneDeck).concat(oneDeck);
 let createImagesArray = () => {
     let imgArray = [];
     for(let j=0; j<156; j++){
         img = new Image();
-        // when j > 52 need to assign a valid image src so take modulo
+        // map deck onto image array
         img.src = `../card_images/${deck[j%52]}.png`;
         imgArray.push(img);
-        console.log(img.src);
     }
     return imgArray;
 }
-
 let shuffle = (deck) => {
     let m = deck.length;
     let currentElement; 
     let index;
-    // While there remain elements to shuffle…
-    while(m){
-        // Pick random element in the lim [0, m-1]
-        index = Math.floor(Math.random() * m--);
-        // let current element be last element
-        currentElement = deck[m];
-        // now swap this last element with a random element
-        deck[m] = deck[index];
-        deck[index] = currentElement;
+    // do randomization process 5 times to shuffle thoroughly
+    for(let j=0; j<5; j++){
+        // While there remain elements to shuffle…
+        while(m>0){
+            // Pick random element in the lim [0, m-1]
+            index = Math.floor(Math.random() * m--);
+            // swap the end element with a random element in array
+            currentElement = deck[m];
+            deck[m] = deck[index];
+            deck[index] = currentElement;
+        }
     }
-    console.log(deck);
     return deck;
 }
-// this produces 3 decks
-let finalDeck = deck.concat(deck).concat(deck);
-
-let deal = () => {
-    shuffle(deck);
-    let imgArray = createImagesArray();
+let deal = (deck, imgArray) => {
+    // the 4 card holder id's for cards to be dealt to start
     let dealID = ["#0", "#1", "#5", "#6"];
     for(let j=0; j<4; j++){
-        let id = dealID[j];
-        // random num between [0, 155] - 3 decks - take %52 because image arry is [0, 52]
-        let rand = Math.floor(Math.random() * 156);
-        $(id).html(imgArray[j]);
-    }    
+        $(dealID[j]).html(imgArray[j]);
+    }
+    imgArray.splice(0, 4);
+    deck.splice(0, 4);
+    console.log(deck);
+}
+let playTurn = (imgArray, counter) => {
+    // the remaining 3 card holders left for player
+    let boxesLeft = ["#7", "#8", "#9"];
+    $(boxesLeft[counter-1]).html(imgArray[0]);
+    imgArray.splice(0, 1);
+    deck.splice(0, 1);
+    console.log(deck);
 }
 $(() => {
-    deal();
+    let counter = 0;
+    shuffle(deck);
+    console.log(deck);
+    let imgArray = createImagesArray();
+    $("#newGame").click(() => {
+        deal(deck, imgArray);
+    })
+    $("#hit").click(() => {
+        counter++;
+        playTurn(imgArray, counter);
+    })
 })
