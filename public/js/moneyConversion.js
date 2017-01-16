6b617949d0e46d2b9304bba4030ee4ab614ac660
Tech-A39 <Tech-A39@rich-0106.uk.brad-ex.co.uk>
2017-01-16 16:55:54 +0000

    Added buttons

--- public/js/moneyConversion.js
+++ public/js/moneyConversion.js
@@ -1,23 +1,47 @@
 
-function makeChange (total, coins) {
-    var tail = coins.slice(0), // clone coins, because we're about to modify it
-        head = tail.shift(); // grab the first coin out of the purse
 
-    // If total is less than zero, or there are no coins left, this isn't a match
-    if (total < 0 || !head) {
-        return 0;
-    }
-    // If the total reached 0, this is a match
-    else if (total === 0 ) {
-        return 1;
-    }
-    // Otherwise, branch
-    else {
-        // The first branch sends the total and a subset of `coins`
-        // The second branch send a reduced total, and all the `coins` 
-        return makeChange(total, tail) + makeChange(total-head, coins);
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function makeChange (total, coins) {
+//     var tail = coins.slice(0), // clone coins, because we're about to modify it
+//         head = tail.shift(); // grab the first coin out of the purse
+
+//     // If total is less than zero, or there are no coins left, this isn't a match
+//     if (total < 0 || !head) {
+//         return 0;
+//     }
+//     // If the total reached 0, this is a match
+//     else if (total === 0 ) {
+//         return 1;
+//     }
+//     // Otherwise, branch
+//     else {
+//         // The first branch sends the total and a subset of `coins`
+//         // The second branch send a reduced total, and all the `coins` 
+//         return makeChange(total, tail) + makeChange(total-head, coins);
+//     }
+// }
 
 
 
