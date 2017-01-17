faa420dedcc854d856361cedb3589e1a2bea7241
Tech-A39 <Tech-A39@rich_0113m.uk.brad-ex.co.uk>
2017-01-17 15:48:26 +0000

    Clicking a chip stacks the chips together

--- public/js/moneyConversion.js
+++ public/js/moneyConversion.js
@@ -3,31 +3,31 @@ $(document).ready(function (){
     $counter = 0;
 
     $("#redChip").click(function(){
-        $('#redChip').clone().appendTo('.chipStack');
+        $('#redChip').clone().appendTo('.chipStack').addClass("chip");
         $counter += 0.05;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#blueChip").click(function(){
-        $('#blueChip').clone().appendTo('.chipStack');
+        $('#blueChip').clone().appendTo('.chipStack').addClass("chip");
         $counter += 0.50;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#orangeChip").click(function(){
-        $('#orangeChip').clone().appendTo('.chipStack');
+        $('#orangeChip').clone().appendTo('.chipStack').addClass("chip");
         $counter += 0.10;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#blackChip").click(function(){
-        $('#blackChip').clone().appendTo('.chipStack');
+        $('#blackChip').clone().appendTo('.chipStack').addClass("chip");
         $counter += 1;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#greenChip").click(function(){
-        $('#greenChip').clone().appendTo('.chipStack');
+        $('#greenChip').clone().appendTo('.chipStack').addClass("chip");
         $counter += 0.20;
         $('#quantity').val($counter.toFixed(2));
     });
