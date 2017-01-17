3dc9dfb25f0bfbfbef72a4e50bc5363721845933
Tech-A37 <Tech-A37@Tech-A37s-MacBook-Air.local>
2017-01-17 12:12:01 +0000

    updating before change to vue

--- public/js/loginVue.js
+++ public/js/loginVue.js
@@ -18,4 +18,5 @@ var app = new Vue({
         }
     }
 });
+
     
--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const registrationController = require("../controllers/registrationController")
 const showGameController = require("../controllers/showGameController")
 const loginController = require("../controllers/loginController")
+const inviteController = require("../controllers/inviteController")
 const homepageController = require("../controllers/homepageController")
 const accountController = require("../controllers/accountController")
 
 
 router.get("/", homepageController.showHomepage);
 router.get("/login", loginController.showLogin);
+router.get("/invite", inviteController.showInvite);
 router.get("/registration", registrationController.showRegistration);
 router.get("/play", showGameController.showGameBoard);
 
