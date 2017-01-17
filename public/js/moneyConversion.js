f266eabb72caa55ee318b7fe85de13a8e7606446
Tech-A39 <Tech-A39@rich_0113m.uk.brad-ex.co.uk>
2017-01-17 15:17:28 +0000

    Clicking the chips add to a total bet

--- public/js/moneyConversion.js
+++ public/js/moneyConversion.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
+<<<<<<< HEAD
     let zIndex = 0;
     let height = 0;
     $counter = 0;
@@ -16,6 +17,13 @@ $(document).ready(function (){
             'z-index' : zIndex++,
             'bottom' : height-=5
         });
+=======
+    let betAmount = 0;
+    $counter = 0;
+
+    $("#redChip").click(function(){
+        $('#redChip').clone().appendTo('.chipStack');
+>>>>>>> Clicking the chips add to a total bet
         $counter += 0.05;
         $('.chipStackBoard').css({
             position: "absolute",
@@ -24,37 +32,53 @@ $(document).ready(function (){
     });
 
     $("#blueChip").click(function(){
+<<<<<<< HEAD
         $('#blueChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
             'bottom' : height+=5
         });
+=======
+        $('#blueChip').clone().appendTo('.chipStack');
+>>>>>>> Clicking the chips add to a total bet
         $counter += 0.50;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#orangeChip").click(function(){
+<<<<<<< HEAD
         $('#orangeChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
             'bottom' : height+=5
         });
+=======
+        $('#orangeChip').clone().appendTo('.chipStack');
+>>>>>>> Clicking the chips add to a total bet
         $counter += 0.10;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#blackChip").click(function(){
+<<<<<<< HEAD
         $('#blackChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
             'bottom' : height+=5
         });
+=======
+        $('#blackChip').clone().appendTo('.chipStack');
+>>>>>>> Clicking the chips add to a total bet
         $counter += 1;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#greenChip").click(function(){
+<<<<<<< HEAD
         $('#greenChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
             'bottom' : height+=5
         });
+=======
+        $('#greenChip').clone().appendTo('.chipStack');
+>>>>>>> Clicking the chips add to a total bet
         $counter += 0.20;
         $('#quantity').val($counter.toFixed(2));
     });
