7595a1b51e789ec01ad79a33e2b914181f30bb98
Tech-A39 <Tech-A39@rich_0113m.uk.brad-ex.co.uk>
2017-01-17 16:39:46 +0000

    Chips stack together from the bottom of the DIV

--- public/js/moneyConversion.js
+++ public/js/moneyConversion.js
@@ -1,33 +1,49 @@
 $(document).ready(function (){
-    let betAmount = 0;
+    let zIndex = 0;
+    let height = 0;
     $counter = 0;
 
     $("#redChip").click(function(){
-        $('#redChip').clone().appendTo('.chipStack').addClass("chip");
+        $('#redChip').clone().appendTo('.chipStack').addClass("chip").css({
+            'z-index' : zIndex++,
+            'bottom' : height+=5
+        });
         $counter += 0.05;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#blueChip").click(function(){
-        $('#blueChip').clone().appendTo('.chipStack').addClass("chip");
+        $('#blueChip').clone().appendTo('.chipStack').addClass("chip").css({
+            'z-index' : zIndex++,
+            'bottom' : height+=5
+        });
         $counter += 0.50;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#orangeChip").click(function(){
-        $('#orangeChip').clone().appendTo('.chipStack').addClass("chip");
+        $('#orangeChip').clone().appendTo('.chipStack').addClass("chip").css({
+            'z-index' : zIndex++,
+            'bottom' : height+=5
+        });
         $counter += 0.10;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#blackChip").click(function(){
-        $('#blackChip').clone().appendTo('.chipStack').addClass("chip");
+        $('#blackChip').clone().appendTo('.chipStack').addClass("chip").css({
+            'z-index' : zIndex++,
+            'bottom' : height+=5
+        });
         $counter += 1;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#greenChip").click(function(){
-        $('#greenChip').clone().appendTo('.chipStack').addClass("chip");
+        $('#greenChip').clone().appendTo('.chipStack').addClass("chip").css({
+            'z-index' : zIndex++,
+            'bottom' : height+=5
+        });
         $counter += 0.20;
         $('#quantity').val($counter.toFixed(2));
     });
