3c4accc7df151c00b1393dde69ee2a72b4c66948
Tech-A39 <Tech-A39@rich_0113m.uk.brad-ex.co.uk>
2017-01-17 15:17:28 +0000

    Clicking the chips add to a total bet

--- public/js/moneyConversion.js
+++ public/js/moneyConversion.js
@@ -1,23 +1,35 @@
 $(document).ready(function (){
+    let betAmount = 0;
+    $counter = 0;
 
     $("#redChip").click(function(){
         $('#redChip').clone().appendTo('.chipStack');
+        $counter += 0.05;
+        $('#quantity').val($counter.toFixed(2));
     });
 
     $("#blueChip").click(function(){
         $('#blueChip').clone().appendTo('.chipStack');
+        $counter += 0.50;
+        $('#quantity').val($counter.toFixed(2));
     });
 
     $("#orangeChip").click(function(){
         $('#orangeChip').clone().appendTo('.chipStack');
+        $counter += 0.10;
+        $('#quantity').val($counter.toFixed(2));
     });
 
     $("#blackChip").click(function(){
         $('#blackChip').clone().appendTo('.chipStack');
+        $counter += 1;
+        $('#quantity').val($counter.toFixed(2));
     });
 
     $("#greenChip").click(function(){
         $('#greenChip').clone().appendTo('.chipStack');
+        $counter += 0.20;
+        $('#quantity').val($counter.toFixed(2));
     });
   
 });
