8ac882d66eeb1b093e0176c3114d44a09401fac3
Tech-A39 <Tech-A39@rich_0113m.uk.brad-ex.co.uk>
2017-01-17 15:48:26 +0000

    Clicking a chip stacks the chips together

--- public/js/moneyConversion.js
+++ public/js/moneyConversion.js
@@ -22,8 +22,12 @@ $(document).ready(function (){
     $counter = 0;
 
     $("#redChip").click(function(){
+<<<<<<< HEAD
         $('#redChip').clone().appendTo('.chipStack');
 >>>>>>> Clicking the chips add to a total bet
+=======
+        $('#redChip').clone().appendTo('.chipStack').addClass("chip");
+>>>>>>> Clicking a chip stacks the chips together
         $counter += 0.05;
         $('.chipStackBoard').css({
             position: "absolute",
@@ -33,6 +37,7 @@ $(document).ready(function (){
 
     $("#blueChip").click(function(){
 <<<<<<< HEAD
+<<<<<<< HEAD
         $('#blueChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
             'bottom' : height+=5
@@ -40,11 +45,15 @@ $(document).ready(function (){
 =======
         $('#blueChip').clone().appendTo('.chipStack');
 >>>>>>> Clicking the chips add to a total bet
+=======
+        $('#blueChip').clone().appendTo('.chipStack').addClass("chip");
+>>>>>>> Clicking a chip stacks the chips together
         $counter += 0.50;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#orangeChip").click(function(){
+<<<<<<< HEAD
 <<<<<<< HEAD
         $('#orangeChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
@@ -53,11 +62,15 @@ $(document).ready(function (){
 =======
         $('#orangeChip').clone().appendTo('.chipStack');
 >>>>>>> Clicking the chips add to a total bet
+=======
+        $('#orangeChip').clone().appendTo('.chipStack').addClass("chip");
+>>>>>>> Clicking a chip stacks the chips together
         $counter += 0.10;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#blackChip").click(function(){
+<<<<<<< HEAD
 <<<<<<< HEAD
         $('#blackChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
@@ -66,11 +79,15 @@ $(document).ready(function (){
 =======
         $('#blackChip').clone().appendTo('.chipStack');
 >>>>>>> Clicking the chips add to a total bet
+=======
+        $('#blackChip').clone().appendTo('.chipStack').addClass("chip");
+>>>>>>> Clicking a chip stacks the chips together
         $counter += 1;
         $('#quantity').val($counter.toFixed(2));
     });
 
     $("#greenChip").click(function(){
+<<<<<<< HEAD
 <<<<<<< HEAD
         $('#greenChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
@@ -79,6 +96,9 @@ $(document).ready(function (){
 =======
         $('#greenChip').clone().appendTo('.chipStack');
 >>>>>>> Clicking the chips add to a total bet
+=======
+        $('#greenChip').clone().appendTo('.chipStack').addClass("chip");
+>>>>>>> Clicking a chip stacks the chips together
         $counter += 0.20;
         $('#quantity').val($counter.toFixed(2));
     });
