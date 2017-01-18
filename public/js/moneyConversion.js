31c1bf6995e17754bf6651de8575bdb983355855
Tech-A39 <Tech-A39@rich-0123.uk.brad-ex.co.uk>
2017-01-18 11:18:09 +0000

    Minor changes to css and html

--- public/js/moneyConversion.js
+++ public/js/moneyConversion.js
@@ -1,14 +1,26 @@
 $(document).ready(function (){
     let zIndex = 0;
     let height = 0;
+    let chipArray = [];
     $counter = 0;
+
+    $("#resetChip").click(function(){
+        //Empty all of the chips in the chipStack div
+        $('.chipStack').empty();
+        height = 0;
+        $counter = 0;
+        $('#quantity').val($counter.toFixed(2));
+    });
 
     $("#redChip").click(function(){
         $('#redChip').clone().appendTo('.chipStack').addClass("chip").css({
             'z-index' : zIndex++,
-            'bottom' : height+=5
+            marginTop : height-=5,
         });
         $counter += 0.05;
+        $('.chipStackBoard').css({
+            position: "absolute",
+        });
         $('#quantity').val($counter.toFixed(2));
     });
 
