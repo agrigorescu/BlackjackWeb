5f2d6662a5678b2802b671774194034d77151e67
andreea grigorescu <deea26@gmail.com>
2017-01-18 16:41:37 +0000

    registratrion works, added the invite friend modal.

---
+++ public/js/components/Modal.vue
@@ -0,0 +1,76 @@
+<template>
+    <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              <div v-if="!formSubmitted">
+                        <form @submit.prevent="validateBeforeSubmit">
+                            <label class="label">Your Email</label>
+                            <p class="control has-icon has-icon-right">
+                                <input name="userEmail" v-model="userEmail" v-validate.initial="userEmail" data-vv-rules="required|email" :class="{'input': true, 'is-danger': errors.has('userEmail') }"
+                                    type="text" placeholder="Your Email">
+                                <i v-show="errors.has('userEmail')" class="fa fa-warning"></i>
+                                <span v-show="errors.has('userEmail')" class="help is-danger">{{ errors.first('userEmail') }}</span>
+                            </p>
+                            <label class="label">Your Friends Email</label>
+                            <p class="control has-icon has-icon-right">
+                                <input name="friendEmail" v-model="friendEmail" v-validate.initial="friendEmail" data-vv-rules="required|email" :class="{'input': true, 'is-danger': errors.has('friendEmail') }"
+                                    type="text" placeholder="Friend Email">
+                                <i v-show="errors.has('friendEmail')" class="fa fa-warning"></i>
+                                <span v-show="errors.has('friendEmail')" class="help is-danger">{{ errors.first('friendEmail') }}</span>
+                            </p>
+                            <button class="btn  waves-effect waves-light right btn-primary btn-block" name="action" id="submitRegistration" type="submit" >Submit</button>
+                        </form>
+                    </div>
+                    <div v-else @click="$emit('close')">
+                        <h1 class="submitted">Form submitted successfully!</h1>
+                    </div>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+    const api = require("../services/api");
+    export default {
+        data: function () {
+                return {
+                    userEmail: '',
+                    friendEmail: '',
+                    formSubmitted: false
+                }
+        },
+        methods: {
+                validateBeforeSubmit(e) {
+                    this.$validator.validateAll();
+                    if (!this.errors.any()) {
+                        this.submitForm()
+                    }
+                },
+                submitForm() {
+                    this.formSubmitted = true;
+                    console.log({ data: { fullName: this.fullName, email: this.email, username: this.username, password: this.password, dob: this.dob } });
+                    api.callApi({ method: 'POST', path: 'https://blackjackapi00.herokuapp.com/friendinvite', params: { userEmail: this.userEmail, friendEmail: this.friendEmail } })
+                        .then(result => {
+                            res.status(201).send({ success: "Done" });
+                            //store token and ID
+                        })
+                        .catch(err => {
+                            res.status(400).send({ sucess: "Not done" });
+                        });
+                }
+        }
+    }
+</script>
--- public/js/pages/AccountInfo.vue
+++ public/js/pages/AccountInfo.vue
@@ -18,14 +18,14 @@
                 </div>
 
                 <!-- Dropdown Trigger -->
-                <a class='dropdown-button btn' id="miniMenu" href='#' data-activates='dropdown1' >Drop Me!</a>
+                <a class='dropdown-button btn' id="miniMenu" href='#' data-activates='dropdown1'>Drop Me!</a>
                 <!-- Dropdown Structure -->
                 <ul id='dropdown1' class='dropdown-content'>
                     <li><a href="#!">one</a></li>
                     <li><a href="#!">two</a></li>
                     <li class="divider"></li>
                     <li><a href="#!">three</a></li>
-                    <li><a data-target="theModal" href="">Invite friends</a></li>
+                    <li><a id="show-modal" @click="showModal = true">Show Modal</a></li>
                 </ul>
                 <button class="btn waves-effect waves-light miniMenu" id="miniMenu2" type="edit">Edit</button>
                 <button class="btn waves-effect waves-light miniMenu" id="miniMenu3" type="other">Other</button>
@@ -33,75 +33,31 @@
             <div id="playButton">PLAY</div>
         </div>
         <!--end of the game board-->
-        <div id="theModal" class="modal">
-            <div class="modal-content">
-                <h4 id="modalHeader">Invite friends</h4>
-                <p id="modalBody">
-                    <div>
-                        <p>Invite your friend over. Type in your email and theirs into the fields below.</p>
-                    </div>
-                    <div v-if="!formSubmitted">
-                        <form @submit.prevent="validateBeforeSubmit">
-                            <label class="label">Your Email</label>
-                            <p class="control has-icon has-icon-right">
-                                <input name="userEmail" v-model="userEmail" v-validate.initial="userEmail" data-vv-rules="required|email" :class="{'input': true, 'is-danger': errors.has('userEmail') }"
-                                    type="text" placeholder="Your Email">
-                                <i v-show="errors.has('userEmail')" class="fa fa-warning"></i>
-                                <span v-show="errors.has('userEmail')" class="help is-danger">{{ errors.first('userEmail') }}</span>
-                            </p>
-                            <label class="label">Your Friends Email</label>
-                            <p class="control has-icon has-icon-right">
-                                <input name="friendEmail" v-model="friendEmail" v-validate.initial="friendEmail" data-vv-rules="required|email" :class="{'input': true, 'is-danger': errors.has('friendEmail') }"
-                                    type="text" placeholder="Friend Email">
-                                <i v-show="errors.has('friendEmail')" class="fa fa-warning"></i>
-                                <span v-show="errors.has('friendEmail')" class="help is-danger">{{ errors.first('friendEmail') }}</span>
-                            </p>
-                            <button class="btn  waves-effect waves-light right btn-primary btn-block" name="action" id="submitRegistration" type="submit">Submit</button>
-                        </form>
-                    </div>
-                    <div v-else>
-                        <h1 class="submitted">Form submitted successfully!</h1>
-                    </div>
-                </p>
-            </div>
-        </div>
+
+        <!--add the modal structure-->
+        <modal v-if="showModal" @close="showModal = false">
+            <!--you can use custom content here to overwrite default content-->
+            <h3 slot="header">custom header</h3>
+        </modal>
     </main-layout>
 </template>
 
 <script>
     import MainLayout from '../layouts/Main.vue'
+    import Modal from '../components/Modal.vue'
 
         const api = require("../services/api");
         export default {
             components: {
-                MainLayout
+                MainLayout,
+                Modal
             },
             data: function () {
                 return {
-                    userEmail: '',
-                    friendEmail: '',
-                    formSubmitted: false
+                    showModal: false
                 }
             },
             methods: {
-                validateBeforeSubmit(e) {
-                    this.$validator.validateAll();
-                    if (!this.errors.any()) {
-                        this.submitForm()
-                    }
-                },
-                submitForm() {
-                    this.formSubmitted = true;
-                    console.log({ data: { fullName: this.fullName, email: this.email, username: this.username, password: this.password, dob: this.dob } });
-                    api.callApi({ method: 'POST', path: 'https://blackjackapi00.herokuapp.com/friendinvite', params: { userEmail: this.userEmail, friendEmail: this.friendEmail } })
-                        .then(result => {
-                            res.status(201).send({ success: "Done" });
-                            //store token and ID
-                        })
-                        .catch(err => {
-                            res.status(400).send({ sucess: "Not done" });
-                        });
-                },
                 openDropdown() {
                         $('.dropdown-button').dropdown({
                             inDuration: 300,
@@ -113,10 +69,7 @@
                             alignment: 'left' // Displays dropdown with edge aligned to the left of button
                         }
                         );
-                    },
-                openModal(){
-                    $('.modal').modal();
-                }
+                    }
             },
             beforeMount: function () {
                 console.log('Mounting');
