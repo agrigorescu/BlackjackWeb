9f13f421ab0bdca0d6e211c0c110f1631f138955
Rafiq Shahsawary <rshahsawary1@hotmail.com>
2017-01-19 16:18:47 +0000

    remove webdriver

--- test/webdriverTest.js
+++ test/webdriverTest.js
@@ -1,13 +1,13 @@
-let webdriver = require("selenium-webdriver");
-let browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
-browser.get('https://en.wikipedia.org/wiki/cats');
+// let webdriver = require("selenium-webdriver");
+// let browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
+// browser.get('https://en.wikipedia.org/wiki/cats');
 
-let theTitle = browser.getTitle();
-theTitle.then((title) => {
-    console.log(title);
-});
+// let theTitle = browser.getTitle();
+// theTitle.then((title) => {
+//     console.log(title);
+// });
 
-browser.findElements(webdriver.By.css('[href^="/wiki/"]')).then((links) => {
-    console.log('Found', links.length, 'Wiki links.' )
-    browser.quit();
-}); 
+// browser.findElements(webdriver.By.css('[href^="/wiki/"]')).then((links) => {
+//     console.log('Found', links.length, 'Wiki links.' )
+//     browser.quit();
+// }); 
